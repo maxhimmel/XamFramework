f2df355e0c21b1c0c9515bd388602ba89a65f4c1
Max Himmel <himmelmax@gmail.com>
2019-10-11 13:33:57 -0700

    Created mini setup helper tool for creating a GameManager prefab!

---
+++ Assets/Scripts/Editor/XamFactory.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Xam.Editor
+{
+	public class XamFactory : EditorWindow
+	{
+		public static GameManager CreateGameManager( string name )
+		{
+			GameObject gameManager = new GameObject( name, typeof( GameManager ) );
+			CreatePrefabAttachment<Level.LevelLoader>( gameManager );
+			CreatePrefabAttachment<Gameplay.TimeManager>( gameManager );
+
+			Audio.AudioManager audioManager = CreateAudioManager( "AudioManager" );
+			audioManager.transform.SetParent( gameManager.transform );
+
+			Initialization.TransitionController transitionController = CreatePrefabAttachment<Initialization.TransitionController>( gameManager );
+			CreatePrefabAttachment<Initialization.FadeTransition>( transitionController.gameObject );
+
+			return gameManager.GetComponent<GameManager>();
+		}
+
+		public static T CreatePrefabAttachment<T>( GameObject prefabParent, string explicitName = null ) where T : Component
+		{
+			string name = explicitName ?? typeof( T ).Name;
+			GameObject newAttachment = new GameObject( name, typeof( T ) );
+			newAttachment.transform.SetParent( prefabParent.transform );
+
+			return newAttachment.GetComponent<T>();
+		}
+
+		private static Audio.AudioManager CreateAudioManager( string name )
+		{
+			GameObject audioManager = new GameObject( name, typeof( Audio.AudioManager ) );
+			AudioSource sfxSource = CreatePrefabAttachment<AudioSource>( audioManager, "SFXSource" );
+			{
+				sfxSource.loop = false;
+				sfxSource.playOnAwake = false;
+			}
+			AudioSource musicSource = CreatePrefabAttachment<AudioSource>( audioManager, "MusicSource" );
+			{
+				musicSource.loop = true;
+				musicSource.playOnAwake = true;
+			}
+
+			SerializedObject audioManagerSerialObj = new SerializedObject( audioManager.GetComponent<Audio.AudioManager>() );
+			{
+				SerializedProperty sfxSourceProperty = audioManagerSerialObj.FindProperty( "m_sfxSource" );
+				sfxSourceProperty.objectReferenceValue = sfxSource;
+				SerializedProperty musicSourceProperty = audioManagerSerialObj.FindProperty( "m_musicSource" );
+				musicSourceProperty.objectReferenceValue = musicSource;
+			}
+			audioManagerSerialObj.ApplyModifiedPropertiesWithoutUndo();
+
+			return audioManager.GetComponent<Audio.AudioManager>();
+		}
+	}
+}
---
+++ Assets/Scripts/Editor/XamSetup.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Xam.Editor
+{
+	public class XamSetup : EditorWindow
+	{
+		[MenuItem( "Xam/Setup/Create GameManager Prefab" )]
+		private static void CreateGameManagerPrefab()
+		{
+			string savePath = GetNewFileSavePath( "Save GameManager Prefab", "GameManager", "prefab" );
+			if ( string.IsNullOrEmpty( savePath ) ) { return; }
+
+			string prefabName = System.IO.Path.GetFileNameWithoutExtension( savePath );
+
+			GameManager gameManager = XamFactory.CreateGameManager( prefabName );
+			GameObject gameManagerObj = gameManager.gameObject;
+
+			SaveAndCleanup( ref gameManagerObj, savePath );
+		}
+
+		private static string GetNewFileSavePath( string panelTitle, string filename, string extension )
+		{
+			string message = $"Create a new {filename} prefab.";
+			return EditorUtility.SaveFilePanelInProject( panelTitle, filename, extension, message );
+		}
+
+		private static bool SaveAndCleanup( ref GameObject prefab, string savePath )
+		{
+			PrefabUtility.SaveAsPrefabAsset( prefab, savePath, out bool success );
+			DestroyImmediate( prefab );
+
+			return success;
+		}
+	}
+}
