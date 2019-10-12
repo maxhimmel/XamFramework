dc52c74a4296c12189496303ed8246c565b0dbf3
Max Himmel <himmelmax@gmail.com>
2019-10-11 20:17:38 -0700

    added dynamic pooling when finding all objects of a certain type is desired, but no pool/factory practice needs to be used

---
+++ Assets/Scripts/Utility/DesignPatterns/Pooling/DynamicPool.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace Xam.Utility.Patterns
+{
+	public class DynamicPool : SingletonMono<DynamicPool>
+	{
+		private Dictionary<System.Type, List<Component>> m_pooledObjectsByType = new Dictionary<System.Type, List<Component>>();
+
+		public IEnumerable<T> GetPooledObjectsByType<T>() where T : Component
+		{
+			if ( m_pooledObjectsByType.TryGetValue( typeof( T ), out List<Component> result ) && result != null )
+			{
+				return result.Cast<T>();
+			}
+			return null;
+		}
+
+		public IEnumerable<T> GetPooledObjectsByType<T>( out int count ) where T : Component
+		{
+			if ( m_pooledObjectsByType.TryGetValue( typeof( T ), out List<Component> result ) && result != null )
+			{
+				count = result.Count;
+				return result.Cast<T>();
+			}
+
+			count = 0;
+			return null;
+		}
+
+		public void AddPooledObjectByType<T>( Component newObj ) where T : Component
+		{
+			if ( newObj == null ) { return; }
+
+			System.Type type = typeof( T );
+			if ( m_pooledObjectsByType.TryGetValue( type, out List<Component> result ) )
+			{
+				result.Add( newObj );
+				return;
+			}
+
+			result = new List<Component>() { newObj };
+			m_pooledObjectsByType[type] = result;
+		}
+
+		public void RemovePooledObjectByType<T>( Component staleObj ) where T : Component
+		{
+			if ( staleObj == null ) { return; }
+
+			System.Type type = typeof( T );
+			if ( m_pooledObjectsByType.TryGetValue( type, out List<Component> result ) )
+			{
+				result.Remove( staleObj );
+				return;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Utility/DesignPatterns/Pooling/DynamicPoolAttachment.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Xam.Utility.Patterns
+{
+	public abstract class DynamicPoolAttachment<T> : MonoBehaviour
+		where T : Component
+	{
+		protected T m_objectInPool = null;
+
+		private void Awake()
+		{
+			m_objectInPool = GetComponent<T>();
+			DynamicPool.Instance.AddPooledObjectByType<T>( m_objectInPool );
+		}
+
+		private void OnDestroy()
+		{
+			DynamicPool.Instance?.RemovePooledObjectByType<T>( m_objectInPool );
+		}
+	}
+}
