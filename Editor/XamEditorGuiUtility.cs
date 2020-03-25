b948675d761a2326b351c4cdcb8cc74eda0ebb3b
Max Himmel <himmelmax@gmail.com>
2020-03-24 22:48:06 -0700

    Created gui utilities + Added texture creation function!

---
+++ Editor/XamEditorGuiUtility.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Xam.Editor
+{
+	public static class XamEditorGuiUtility
+	{
+		public static Texture2D CreateTexture( int width, int height, Color color )
+		{
+			Color[] pixels = new Color[width * height];
+
+			for ( int i = 0; i < pixels.Length; i++ )
+			{
+				pixels[i] = color;
+			}
+
+			Texture2D result = new Texture2D( width, height );
+			result.SetPixels( pixels );
+			result.Apply();
+
+			return result;
+		}
+	}
+}
