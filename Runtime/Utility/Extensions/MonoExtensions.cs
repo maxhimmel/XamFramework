11d287ed23c062200033d6f79b1536c485f1f303
Max Himmel <himmelmax@gmail.com>
2020-05-02 11:24:32 -0700

    Added wait for seconds realtime in mono extension

--- Runtime/Utility/Extensions/MonoExtensions.cs
+++ Runtime/Utility/Extensions/MonoExtensions.cs
@@ -11,16 +11,28 @@ namespace Xam.Utility.Extensions
 			return mono.StartCoroutine( WaitForSeconds_Coroutine( seconds, callback ) );
 		}
 
-		public static Coroutine StartWaitingForFrames( this MonoBehaviour mono, int frames, System.Action callback )
-		{
-			return mono.StartCoroutine( WaitForFrames_Coroutine( frames, callback ) );
-		}
-
 		public static IEnumerator WaitForSeconds_Coroutine( float seconds, System.Action callback )
 		{
 			if ( seconds > 0 ) { yield return new WaitForSeconds( seconds ); }
 
 			callback?.Invoke();
+		}
+
+		public static Coroutine StartWaitingForUnscaledSeconds( this MonoBehaviour mono, float seconds, System.Action callback )
+		{
+			return mono.StartCoroutine( WaitForUnscaledSeconds_Coroutine( seconds, callback ) );
+		}
+
+		public static IEnumerator WaitForUnscaledSeconds_Coroutine( float seconds, System.Action callback )
+		{
+			if ( seconds > 0 ) { yield return new WaitForSecondsRealtime( seconds ); }
+
+			callback?.Invoke();
+		}
+
+		public static Coroutine StartWaitingForFrames( this MonoBehaviour mono, int frames, System.Action callback )
+		{
+			return mono.StartCoroutine( WaitForFrames_Coroutine( frames, callback ) );
 		}
 
 		public static IEnumerator WaitForFrames_Coroutine( int frames, System.Action callback )
