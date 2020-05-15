33ac99bdc8e7e1e718ff70b2c8ff794a2af30994
Max Himmel <himmelmax@gmail.com>
2020-05-14 22:42:21 -0700

    + Added camera extension for getting orthographic world-bounds

---
+++ Runtime/Utility/Extensions/CameraExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Xam.Utility.Extensions
+{
+	public static class CameraExtensions
+	{
+		/// <summary>
+		/// Returned bounds is centered at <paramref name="camera"/>'s transform position.
+		/// </summary>
+		/// <param name="camera"></param>
+		/// <returns></returns>
+		public static Bounds GetOrthoWorldBounds( this Camera camera )
+		{
+			float orthoHeight = camera.orthographicSize * 2;
+			float orthoWidth = camera.aspect * orthoHeight;
+
+			Vector3 orthoWorldSize = new Vector3( orthoWidth, orthoHeight );
+			return new Bounds( camera.transform.position, orthoWorldSize );
+		}
+	}
+}
