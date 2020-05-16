a1183ecd3047a573edf834c728f1deb122191a74
Max Himmel <himmelmax@gmail.com>
2020-05-16 12:17:38 -0700

    + Added script field drawing for custom editors

--- Editor/XamEditorGuiUtility.cs
+++ Editor/XamEditorGuiUtility.cs
@@ -1,11 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 namespace Xam.Editor
 {
 	public static class XamEditorGuiUtility
 	{
+		public static void ScriptFieldLayout( SerializedObject serializedObject, params GUILayoutOption[] options )
+		{
+			ScriptField( EditorGUILayout.GetControlRect( options ), serializedObject );
+		}
+
+		public static void ScriptField( Rect position, SerializedObject serializedObject )
+		{
+			GUI.enabled = false;
+			{
+				SerializedProperty scriptProperty = serializedObject.FindProperty( "m_Script" );
+				EditorGUI.PropertyField( position, scriptProperty, true );
+			}
+			GUI.enabled = true;
+		}
+
 		public static Texture2D CreateTexture( int width, int height, Color color )
 		{
 			Color[] pixels = new Color[width * height];
