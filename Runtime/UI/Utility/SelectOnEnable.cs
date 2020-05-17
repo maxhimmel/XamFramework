0523e70af0a018da047a2e743ef33f8cb5d81331
Max Himmel <himmelmax@gmail.com>
2020-05-17 13:23:34 -0700

    + Added UI helper for selecting buttons when using controllers

---
+++ Runtime/UI/Utility/SelectOnEnable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+namespace Xam.Ui
+{
+	[RequireComponent( typeof( Selectable ) )]
+	public class SelectOnEnable : MonoBehaviour
+	{
+		private Selectable m_selectable = null;
+
+		private void OnEnable()
+		{
+			if ( m_selectable == null )
+			{
+				m_selectable = GetComponent<Selectable>();
+			}
+
+			if ( EventSystem.current != null )
+			{
+				EventSystem.current.SetSelectedGameObject( m_selectable.gameObject );
+			}
+		}
+	}
+}
