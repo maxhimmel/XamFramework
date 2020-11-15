cf8830381e475b9a7610ad9fb1460cdfa2f1ba44
Max Himmel <himmelmax@gmail.com>
2020-11-15 12:01:14 -0800

    Helper for selecting objects in scene easier

---
+++ Runtime/Utility/SelectionBase.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Xam.Utility
+{
+	[SelectionBase]
+	public class SelectionBase : MonoBehaviour
+	{
+	}
+}
