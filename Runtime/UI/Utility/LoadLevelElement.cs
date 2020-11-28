810eb67106936a83498d4308407157d2bfdff7f5
Max Himmel <himmelmax@gmail.com>
2020-11-28 12:32:06 -0800

    + Added button helpers for quickly loading/exiting game modes

---
+++ Runtime/UI/Utility/LoadLevelElement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Xam.Ui
+{
+	using Level;
+
+	public class LoadLevelElement : MonoBehaviour
+	{
+		public void LoadLevel( string levelName )
+		{
+			if ( GameManager.Exists )
+			{
+				GameManager.Instance.LoadLevel( levelName );
+			}
+		}
+	}
+}
---
+++ Runtime/UI/Utility/QuitGameElement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Xam.Ui
+{
+	public class QuitGameElement : MonoBehaviour
+	{
+		public void Quit()
+		{
+			Application.Quit();
+
+#if UNITY_EDITOR
+			UnityEditor.EditorApplication.ExitPlaymode();
+#endif
+		}
+	}
+}
