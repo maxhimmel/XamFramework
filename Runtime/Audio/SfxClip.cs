db590f8973e2952d92c1e3902b54cd332ad12786
Max Himmel <himmelmax@gmail.com>
2020-11-30 20:10:58 -0800

    + Added gizmo drawing for SFX distance + SFX can now play on start

--- Runtime/Audio/SfxClip.cs
+++ Runtime/Audio/SfxClip.cs
@@ -6,7 +6,18 @@ namespace Xam.Audio
 {
 	public class SfxClip : MonoBehaviour
 	{
-		[SerializeField] private SoundDatum m_soundData = default;
+		[SerializeField] private bool m_playOnStart = false;
+
+		[Space]
+		[SerializeField] private SoundDatum m_soundData = new SoundDatum();
+
+		private void Start()
+		{
+			if ( m_playOnStart )
+			{
+				PlaySfx();
+			}
+		}
 
 		[ContextMenu( "Play SFX" )]
 		public virtual void PlaySfx()
@@ -15,5 +26,24 @@ namespace Xam.Audio
 
 			AudioManager.Instance.PlaySound( m_soundData );
 		}
+
+
+#if UNITY_EDITOR
+		private void OnDrawGizmosSelected()
+		{
+			if ( m_soundData.Is3d )
+			{
+				Transform center = transform;
+				if ( m_soundData.Attachment3d.Attachment != null )
+				{
+					center = m_soundData.Attachment3d.Attachment;
+				}
+
+				Gizmos.color = Color.white;
+				Gizmos.DrawWireSphere( center.position, m_soundData.Attachment3d.MaxDistance );
+				Gizmos.DrawWireSphere( center.position, m_soundData.Attachment3d.MinDistance );
+			}
+		}
+#endif
 	}
 }
