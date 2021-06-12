29103d09254ed126dde4772707b618b6e14a176b
Max Himmel <himmelmax@gmail.com>
2021-06-12 10:34:01 -0700

    + Added default weighted list for gameobjects for easy plug-and-play w/other repos

---
+++ Runtime/Utility/Randomization/GameObjectWeightedList.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Xam.Utility.Randomization
+{
+	[System.Serializable]
+	public class GameObjectWeightedNode : WeightedNode<GameObject> { }
+
+	[System.Serializable]
+	public class GameObjectWeightedList : WeightedList<GameObjectWeightedNode, GameObject> { }
+}
