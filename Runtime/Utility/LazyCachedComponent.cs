04562f465539300206321bd779ade0b6625c0e4c
Max Himmel <himmelmax@gmail.com>
2021-07-22 07:10:15 -0700

    + Helpful component getter - w/lazy initialization

---
+++ Runtime/Utility/LazyCachedComponent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Xam.Utility
+{
+	public class LazyCachedComponent<T>
+		where T : class
+	{
+		public T this[Component root] 
+		{
+			get 
+			{
+				return this[root.gameObject];
+			}
+		}
+
+		public T this[GameObject root] 
+		{
+			get 
+			{
+				if ( m_cachedComponent != null ) { return m_cachedComponent; }
+
+				m_cachedComponent = GetComponent( root );
+				return m_cachedComponent;
+			}
+		}
+
+		private T m_cachedComponent = null;
+
+		protected virtual T GetComponent( GameObject root )
+		{
+			return root.GetComponent<T>();
+		}
+	}
+}
