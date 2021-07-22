366e8eab474e75aeb0f1b1ef04ca5c7d5b084cd6
Max Himmel <himmelmax@gmail.com>
2021-07-22 09:02:58 -0700

    + Camera finding helpers!

---
+++ Runtime/Gameplay/Camera/CineBrainCameraFinder.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+namespace Xam.Gameplay.Cameras
+{
+	public class CineBrainCameraFinder : MonoBehaviour, ICameraFinder
+	{
+		[SerializeField, Min( 0 )] private int m_cineBrainIndex = 0;
+
+		private Camera m_camera = null;
+
+		Camera ICameraFinder.GetCamera()
+		{
+			if ( m_camera != null ) { return m_camera; }
+
+			CacheCamera();
+			return m_camera;
+		}
+
+		private void CacheCamera()
+		{
+			CinemachineCore cineCore = CinemachineCore.Instance;
+			if ( cineCore != null && cineCore.BrainCount > 0 )
+			{
+				CinemachineBrain brain = cineCore.GetActiveBrain( m_cineBrainIndex );
+				if ( brain != null )
+				{
+					m_camera = brain.OutputCamera;
+				}
+			}
+		}
+	}
+}
---
+++ Runtime/Gameplay/Camera/MainCameraFinder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Xam.Gameplay.Cameras
+{
+	public class MainCameraFinder : MonoBehaviour, ICameraFinder
+	{
+		private Camera m_camera = null;
+
+		Camera ICameraFinder.GetCamera()
+		{
+			if ( m_camera != null ) { return m_camera; }
+
+			CacheCamera();
+			return m_camera;
+		}
+
+		private void CacheCamera()
+		{
+			m_camera = Camera.main;
+		}
+	}
+}
