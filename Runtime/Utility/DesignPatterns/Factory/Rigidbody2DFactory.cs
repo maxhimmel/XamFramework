20eba25bc0370acf859a81d46c86132fd1664024
Max Himmel <himmelmax@gmail.com>
2021-09-25 08:17:55 -0700

    + Created another lazy ccomponent cahe helper for child components + Added rigidbody 2d factory

---
+++ Runtime/Utility/DesignPatterns/Factory/Rigidbody2DFactory.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Xam.Gameplay.Patterns
+{
+	public class Rigidbody2DFactory : Factory<Rigidbody2D>
+	{
+	}
+}
---
+++ Runtime/Utility/LazyComponentCaching/LazyCachedChildComponent.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Xam.Utility
+{
+	public class LazyCachedChildComponent<T> : LazyCachedComponent<T>
+		where T : class
+	{
+		private readonly bool k_includeInactiveChildren;
+
+		public LazyCachedChildComponent(bool includeInactiveChildren)
+		{
+			k_includeInactiveChildren = includeInactiveChildren;
+		}
+
+		protected override T GetComponent( GameObject root )
+		{
+			return root.GetComponentInChildren<T>( k_includeInactiveChildren );
+		}
+	}
+}
