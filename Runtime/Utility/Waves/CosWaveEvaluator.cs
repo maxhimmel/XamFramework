2a795e0642499b7f3554475c42f29821d3e103d9
Max Himmel <himmelmax@gmail.com>
2021-09-27 21:22:35 -0700

    + Added sinusoidal light framework

---
+++ Runtime/Utility/Waves/CosWaveEvaluator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Xam.Utility
+{
+	public class CosWaveEvaluator : MonoBehaviour, IWaveEvaluator
+	{
+		public float Evaluate( WaveDatum data, float time )
+		{
+			return data.Amplitude * Mathf.Cos( time * data.Frequency + data.Phase );
+		}
+	}
+}
